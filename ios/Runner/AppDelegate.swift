32153216af8a2fa7a8ea987745680a4f038bb352
Amelio <amelio.apaza@adventistas.org>
2022-03-03 05:50:57 -0500

    changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 import Flutter
 
 @UIApplicationMain
@@ -8,6 +9,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    if FirebaseApp.app() == nil {
+      FirebaseApp.configure()
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
