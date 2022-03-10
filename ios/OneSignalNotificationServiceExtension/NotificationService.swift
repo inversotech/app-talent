387ba3ef31af9e54494c74a88cd5e7d446333c78
Amelio <amelio.apaza@adventistas.org>
2022-03-10 14:19:32 -0500

    Cambios

--- ios/OneSignalNotificationServiceExtension/NotificationService.swift
+++ ios/OneSignalNotificationServiceExtension/NotificationService.swift
@@ -2,7 +2,7 @@
 //  NotificationService.swift
 //  OneSignalNotificationServiceExtension
 //
-//  Created by Amelio Apaza Humerez on 2/03/22.
+//  Created by Amelio Apaza Humerez on 4/03/22.
 //
 
 import UserNotifications
@@ -18,15 +18,14 @@ class NotificationService: UNNotificationServiceExtension {
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.receivedRequest = request
         self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        self.bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
         if let bestAttemptContent = bestAttemptContent {
             // Modify the notification content here...
-
             // bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
             
             // contentHandler(bestAttemptContent)
-
+            
             OneSignal.didReceiveNotificationExtensionRequest(self.receivedRequest, with: bestAttemptContent, withContentHandler: self.contentHandler)
         }
     }
