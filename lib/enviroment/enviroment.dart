83b8d6fc1994a97e5b7baf26f9c3a56dab8a294c
Ulises Julca <ulises@adventistas.pe>
2022-12-15 15:59:56 -0500

    se mejoró los enviroments

--- lib/enviroment/enviroment.dart
+++ lib/enviroment/enviroment.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/foundation.dart';
 import 'package:lamb_talent/enviroment/models.dart';
 import 'package:lamb_talent/enviroment/vhosts.dart';
 
 class Env {
-  static VHost api = kDebugMode ? Api().apiDev : Api().apiProd;
+  static VHost api =  Api().apiProd;
 }
