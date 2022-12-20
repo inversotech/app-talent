64e1212123b54c49eda857959e5f13621f9a1727
Ulises Julca <ulises@adventistas.pe>
2022-12-20 14:20:48 -0500

    probando ubicacion

--- lib/core/location_user.dart
+++ lib/core/location_user.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:lamb_talent/core/user_preferences.dart';
 
@@ -47,10 +49,38 @@ class LocationUser {
 
   changeLocationUser() {
     final userPreferences = UserPreferences();
-    const LocationSettings locationSettings = LocationSettings(
-      accuracy: LocationAccuracy.best,
-      distanceFilter: 5,
-    );
+   late LocationSettings locationSettings;
+
+if (Platform.isAndroid) {
+  locationSettings = AndroidSettings(
+    accuracy: LocationAccuracy.high,
+    distanceFilter: 5,
+    forceLocationManager: true,
+    intervalDuration: const Duration(seconds: 10),
+    //(Optional) Set foreground notification config to keep the app alive 
+    //when going to the background
+    foregroundNotificationConfig: const ForegroundNotificationConfig(
+        notificationText:
+        "Example app will continue to receive your location even when you aren't using it",
+        notificationTitle: "Running in Background",
+        enableWakeLock: true,
+    )
+  );
+} else if (Platform.isIOS || Platform.isMacOS) {
+  locationSettings = AppleSettings(
+    accuracy: LocationAccuracy.high,
+    activityType: ActivityType.fitness,
+    distanceFilter: 5,
+    pauseLocationUpdatesAutomatically: true,
+    // Only set to true if our app will be started up in the background.
+    showBackgroundLocationIndicator: false,
+  );
+} else {
+    locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high,
+    distanceFilter: 5,
+  );
+}
     Geolocator.getPositionStream(locationSettings: locationSettings)
             .listen((Position? position) {
       if (position != null) {
